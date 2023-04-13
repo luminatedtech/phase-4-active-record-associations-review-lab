7ad8ed8ead01c6c54733e5b65443e1f7684ee740
Allen Kim <avalmercafe02@gmail.com>
2023-04-13 17:44:48 -0400

    commit tree de553324c124e437bb097d0fb5537f291ae71137

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer :passenger_id, foreign_key: true
+      t.integer :taxi_id, foreign_key: true
       t.timestamps
     end
   end
